6cf7c5750d4e5f34b80c7364741433042545f309
Binary-Rabbit <ellaine.simpkin@gmail.com>
2023-05-10 14:35:36 +1200

    Format DB sample files
    Edited the sample SQL files to add formatted sample data
    
    Co-Authored-By: Guy <guykibby@gmail.com>
    Co-Authored-By: Mitchellvari <103621930+Mitchellvari@users.noreply.github.com>
    Co-Authored-By: Angus <89812891+azhishchenkov@users.noreply.github.com>

--- db/sql/01_database.sql
+++ db/sql/01_database.sql
@@ -1,4 +1,14 @@
-CREATE TABLE form(
-  id SERIAL PRIMARY KEY,
-  field_1 VARCHAR(255)
-);
+CREATE TABLE
+  form (
+    claim_id SERIAL PRIMARY KEY,
+    policy_number INT NOT NULL,
+    customer_id VARCHAR NOT NULL,
+    condition_claimed_for TEXT NOT NULL,
+    first_symptoms_date DATE NOT NULL,
+    symptoms_details TEXT NOT NULL,
+    medical_service_type VARCHAR(255) NOT NULL,
+    service_provider_name VARCHAR(255) NOT NULL,
+    other_insurance_provider BOOLEAN DEFAULT false,
+    consent BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT NOW ()
+  );
--- db/sql/02_test_data.sql
+++ db/sql/02_test_data.sql
@@ -1,2 +1,49 @@
-INSERT INTO  form (field_1)
-VALUES ('test value 1'), ('test value 2');
+INSERT INTO
+    form (
+        policy_number,
+        customer_id,
+        condition_claimed_for,
+        first_symptoms_date,
+        symptoms_details,
+        medical_service_type,
+        service_provider_name,
+        other_insurance_provider,
+        consent
+    )
+VALUES
+    (
+        1234567890,
+        'CUST001',
+        'Back pain',
+        '2022-01-01',
+        'Experienced sharp pain while lifting heavy object',
+        'Physical therapy',
+        'ABC Medical Center',
+        false,
+        true
+    );
+
+INSERT INTO
+    form (
+        policy_number,
+        customer_id,
+        condition_claimed_for,
+        first_symptoms_date,
+        symptoms_details,
+        medical_service_type,
+        service_provid ider_name,
+        other_insurance_provider,
+        consent
+    )
+VALUES
+    (
+        2345678901,
+        'CUST002',
+        'Heart attack',
+        '2022-02-15',
+        'Chest pain, dizziness, and shortness of breath',
+        'Cardiology',
+        'XYZ Hospital',
+        true,
+        true
+    );
