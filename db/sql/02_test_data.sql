08f4087c81af2f7d408768b838643b6460ed7fc0
Binary-Rabbit <ellaine.simpkin@gmail.com>
2023-05-10 14:41:35 +1200

    Remove superfluous data

--- db/sql/02_test_data.sql
+++ db/sql/02_test_data.sql
@@ -22,28 +22,3 @@ VALUES
         false,
         true
     );
-
-INSERT INTO
-    form (
-        policy_number,
-        customer_id,
-        condition_claimed_for,
-        first_symptoms_date,
-        symptoms_details,
-        medical_service_type,
-        service_provid ider_name,
-        other_insurance_provider,
-        consent
-    )
-VALUES
-    (
-        2345678901,
-        'CUST002',
-        'Heart attack',
-        '2022-02-15',
-        'Chest pain, dizziness, and shortness of breath',
-        'Cardiology',
-        'XYZ Hospital',
-        true,
-        true
-    );
