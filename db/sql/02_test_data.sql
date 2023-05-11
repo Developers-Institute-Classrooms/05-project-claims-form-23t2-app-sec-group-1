d8c36f352f3660894f1b831f8004f8a00d32a29c
Guy <guykibby@gmail.com>
2023-05-11 10:20:47 +1000

    Add SQL value parameters to claims table
    Co-Authored-By: Ellaine <ellaine.simpkin@developersinstitute.co.nz>
    Co-Authored-By: Mitchellvari <103621930+Mitchellvari@users.noreply.github.com>
    Co-Authored-By: Angus <89812891+azhishchenkov@users.noreply.github.com>

--- db/sql/02_test_data.sql
+++ db/sql/02_test_data.sql
@@ -1,24 +1,24 @@
-INSERT INTO
-    form (
-        policy_number,
-        customer_id,
-        condition_claimed_for,
-        first_symptoms_date,
-        symptoms_details,
-        medical_service_type,
-        service_provider_name,
-        other_insurance_provider,
+  INSERT INTO
+    Claims (
+         policy_number,
+         customer_id,
+         condition_claimed_for,
+         first_symptoms_date,
+         symptoms_details,
+         medical_service_type,
+         service_provider_name,
+         other_insurance_provider,
         consent
     )
 VALUES
     (
-        1234567890,
-        'CUST001',
-        'Back pain',
-        '2022-01-01',
-        'Experienced sharp pain while lifting heavy object',
-        'Physical therapy',
-        'ABC Medical Center',
-        false,
+         '12345678',
+         'CUST001',
+         'Back pain',
+         '2022-01-01',
+         'Experienced sharp pain while lifting heavy object',
+         'Physical therapy',
+         'ABC Medical Center',
+         false,
         true
     );
