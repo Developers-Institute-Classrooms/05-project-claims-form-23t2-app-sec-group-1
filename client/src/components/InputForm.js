e5d92c628c5b3dd21a67e6e4d967bc7221b7a78f
Mitchell Vari <mitchellvari@developersinstitute.co.nz>
2023-05-11 14:56:35 +1200

    Update input form - guy, mitchell, angus

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -5,11 +5,18 @@ const InputForm = () => {
   const [policyNumber, setPolicyNumber] = useState("");
   const [customerId, setCustomerId] = useState("");
   const [conditionClaimedFor, setConditionClaimedFor] = useState("");
+<<<<<<< HEAD
   const [startDate, setStartDate] = useState(new Date());
   const [symptomsDetails, setSymptomsDetails] = useState("");
   const [medicalServiceType, setMedicalServiceType] = useState("");
   const [serviceProviderName, setServiceProviderName] = useState("");
   const [otherInsuranceProvider, setOtherInsuranceProvider] = useState("");
+=======
+  const [symptomsDetails, setSymptomsDetails] = useState("");
+  const [medicalServiceType, setMedicalServiceType] = useState("");
+  const [serviceProviderName, setServiceProviderName] = useState("");
+  const [otherInsuranceProvider, setOtherInsuranceProvider] = useState(true);
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
   const [isChecked, setIsChecked] = useState(false);
 
   const onSubmit = async () => {
@@ -40,49 +47,87 @@ const InputForm = () => {
     <Fragment>
       <h1 className="text-center mt-5">Form</h1>
       <div className="d-flex mt-5">
+        <label htmlFor="policyNumber">Policy Number</label>
         <input
+          id="policyNumber"
           type="text"
           className="form-control"
           value={policyNumber}
           onChange={(e) => setPolicyNumber(e.target.value)}
         />
+<<<<<<< HEAD
         <input
+=======
+        <label htmlFor="customerId">Customer Id</label>
+        <input
+          id="customerId"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           type="text"
           className="form-control"
           value={customerId}
           onChange={(e) => setCustomerId(e.target.value)}
         />
+<<<<<<< HEAD
         <input
+=======
+        <label htmlFor="conditionClaimedFor">Condition Claimed For</label>
+        <input
+          id="conditionClaimedFor"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           type="text"
           className="form-control"
           value={conditionClaimedFor}
           onChange={(e) => setConditionClaimedFor(e.target.value)}
         />
+<<<<<<< HEAD
         <DatePicker
           selected={startDate}
           onChange={(date) => setStartDate(date)}
         />
         <input
+=======
+        <label htmlFor="symptomDetails">Symptom Details</label>
+        <input
+          id="symptomDetails"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           type="text"
           className="form-control"
           value={symptomsDetails}
           onChange={(e) => setSymptomsDetails(e.target.value)}
         />
+<<<<<<< HEAD
         <input
+=======
+        <label htmlFor="medicalServiceType">Medical Service Type</label>
+        <input
+          id="medicalServiceType"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           type="text"
           className="form-control"
           value={medicalServiceType}
           onChange={(e) => setMedicalServiceType(e.target.value)}
         />
+<<<<<<< HEAD
 
         <input
+=======
+        <label htmlFor="serviceProviderName">Service Provider Name</label>
+        <input
+          id="serviceProviderName"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           type="text"
           className="form-control"
           value={serviceProviderName}
           onChange={(e) => setServiceProviderName(e.target.value)}
         />
+<<<<<<< HEAD
 
         <select
+=======
+        <label htmlFor="otherInsuranceProvider">Other Insurance Provider</label>
+        <select
+          id="otherInsuranceProvider"
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
           className="selectBox"
           value={otherInsuranceProvider}
           onChange={(e) => setOtherInsuranceProvider(e.target.value)}
--- server/routes/form-router.js
+++ server/routes/form-router.js
@@ -5,7 +5,7 @@ const formRouter = express.Router();
 formRouter.get("/", async (req, res) => {
   console.log("hot reloaded!");
   try {
-    const allItems = await pool.query("SELECT * FROM form");
+    const allItems = await pool.query("SELECT * FROM Claims");
     res.json(allItems.rows);
   } catch (err) {
     console.error(err.message);
@@ -15,10 +15,21 @@ formRouter.get("/", async (req, res) => {
 formRouter.post("/form", async (req, res) => {
   console.log(req.body);
   try {
+<<<<<<< HEAD
     const allItems =
       await pool.query(`INSERT INTO form (policy_number, customer_id, condition_claimed_for,first_symptoms_date,symptoms_details,medical_service_type,service_provider_name,other_insurance_provider,consent)
         VALUES (value1, value2, value3)`);
     res.json(allItems.rows);
+=======
+    console.log(req.body);
+    // const { name, email, message } = req.body;
+    const newItem = await pool.query(
+      `INSERT INTO Claims (policy_number, customer_id, condition_claimed_for,first_symptoms_date,symptoms_details,medical_service_type,service_provider_name,other_insurance_provider,consent)
+        VALUES ('$1','$2', '$3', '$4', '$5', '$6','$7','$8','$9'),[]`
+        );
+
+    res.json("hello there");
+>>>>>>> ad2a4df (Update input form - guy, mitchell, angus)
   } catch (err) {
     console.error(err.message);
   }
