4b777731c54ae3c808509c2e22ef37b572d2ddfe
Guy <guykibby@gmail.com>
2023-05-11 10:15:51 +1000

    add SQL adjustments

--- db/sql/01_database.sql
+++ db/sql/01_database.sql
@@ -1,7 +1,26 @@
+CREATE FUNCTION unique_random()
+RETURNS bigint AS $$
+DECLARE
+    r bigint;
+BEGIN
+    LOOP
+        -- Generate a random 10-digit number
+        r := floor(random() * (9999999999 - 1000000000 + 1) + 1000000000)::bigint;
+
+        -- Check if this number already exists in the table
+        IF NOT EXISTS(SELECT 1 FROM Claims WHERE claim_id = r) THEN
+            RETURN r;
+        END IF;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE TABLE
-  form (
-    claim_id SERIAL PRIMARY KEY,
-    policy_number INT NOT NULL,
+  Claims (
+    claim_id BIGINT DEFAULT unique_random() PRIMARY KEY,
+    status VARCHAR(255) DEFAULT 'submitted',
+    policy_number CHAR(8) CHECK (policy_number SIMILAR TO '[0-9]{8}'),
     customer_id VARCHAR NOT NULL,
     condition_claimed_for TEXT NOT NULL,
     first_symptoms_date DATE NOT NULL,
