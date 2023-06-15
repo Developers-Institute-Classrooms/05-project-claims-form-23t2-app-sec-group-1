1c6ef901260ee5ee3a6905d9a92daeb0086924ac
Azhishchenkov <angusmcirvine@developersinstitute.co.nz>
2023-06-15 21:19:42 +1200

    db.js updated, formRouter updated for CryptoJS

--- server/db.js
+++ server/db.js
@@ -1,5 +1,10 @@
-const Pool = require("pg").Pool;
+const { Pool } = require('pg');
+
 const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
 });
 
 module.exports = pool;
